c404fc59485f3ce1d73a4124cd1b20807883285c
oropo015 <oriolowoopeyemi@yahoo.com>
2025-05-10 19:16:06 +0100

    standardize

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone', // Enable standalone output
   swcMinify: true,
   eslint: {
     ignoreDuringBuilds: true,
